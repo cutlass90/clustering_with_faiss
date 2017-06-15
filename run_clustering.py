b7ff8683b8183ef4522cf34f3a88d0aa4214dcb4
nazars <cutlass90@mail.ru>
2017-06-15 17:49:06 +0300

    modify skript to plot cluster variance

--- run_clustering.py
+++ run_clustering.py
@@ -11,162 +11,32 @@ import ecg
 from ecg.utils import tools
 from ecg.utils.diseases import all_holter_diseases
 
-def plot_ecg(channels,  samples_per_pic, save_path, file_name,
+def plot_ecg(channels,  samples_per_pic, save_path, file_name, fig_settings,
             first_sample=None, last_sample=None, channel_names=['ES', 'AS', 'AI'],
             convert_from_easi=True, beats=None, events=None, diseases=None,
             predicted_diseases=None, predicted_events=None, predicted_beats=None,
             probabilities=None, verbose=False, not_plot_good_chunks=False,
             max_failed_plots=100):
 
-    """Plots channels with provided beats and/or events. Saves figures to 
-        save_path/file_name/
-
-    Args:
-        channels: list on channels to plot.
-        first_sample: sample number to start plotting from. If not specified, 
-            defaults to the first sample of the current file.
-        last_sample: last sample to plot. If not specified, defaults to
-            the last sample of the current file.
-        samples_per_pic: number of sample per picture, 2-4k is recommended
-        save_path: folder to save pics in. Will be created automatically.
-        file_name: name of file.
-        beats: array of beats positions is samples starting from 0.
-        events: events if form of numpy array of shape (?, n_diseases). Each
-            event should correspond to a beat.
-        diseases: list of diseases. Length should be the same as second dimension
-            of events array.
-        predicted_events: same as events.
-        predicted_beats: same as beats.
-        probabilities: probabilities of predicted events in norm on numpy array
-            of shape (?, n_diseases).
-        verbose: whether to print progress.
-        not_plot_good_chunks: whether or not to plot chunks with correct predictions.
-            Require both `events` and `predicted events`. 
-        max_failed_plots: maximum number of figures with incorrect predictions.
-            Used only when `not_plot_good_chunks` is True.
-
-    Raises:
-        ValueError: if some arrays have inconsistent dimensions
-             (e.g. events don't correspond to beats).
-    """
-
-    if verbose:
-        print('Plotting {}.'.format(file_name))
-
-    if events is not None:
-        if events.shape[1] != len(diseases):
-            raise ValueError('Each element of event should correspond to a particular disease.')
-        if beats is None or (len(events) != len(beats)):
-            raise ValueError('Events could not be plotted without corresponding beats.')
-        if predicted_events is not None:
-            if len(predicted_events) != len(events):
-                raise ValueError('Each predicted event should correspond to a true event.')
-            if predicted_events.shape[1] != len(predicted_diseases):
-                raise ValueError('Each element of predicted event should correspond to a particular disease.')
-        if probabilities is not None:
-            if len(probabilities) != len(events):
-                raise ValueError('Each probability of event should correspond to a true event.')
-
-    tools.maybe_create_dirs(os.path.join(save_path, file_name))
-    if channel_names != ['ES', 'AS', 'AI']:
-        if convert_from_easi:
-            channels = tools.convert_channels_from_easi(channels, channel_names)
-
     channel_length = len(channels[0])
-    if last_sample is None or last_sample > channel_length:
-        last_sample = channel_length
+    call_number = fig_settings[2]
 
-    if first_sample is None:
-        first_sample = 0
+    t = np.arange(0, (last_sample-first_sample))*1000/175
 
-    start = first_sample
-    end = start + samples_per_pic
+    fig = fig_settings[0]
+    subplots =fig_settings[1]
 
-    if not_plot_good_chunks:
-        if all(arg is not None for arg in [events, predicted_events, beats]):
-            fails = beats[np.not_equal(events, predicted_events).any(axis=1)]
-            print('{n_fails} fails total.'.format(n_fails=len(fails)))
-            if len(fails) > max_failed_plots:
-                fails = np.random.choice(fails, max_failed_plots, replace=False)
-        else:
-            raise ValueError('To skip good chunks, `events`,' \
-                ' `predicted_events` and `beats` should be provided.')
+    channel_name = iter(channel_names)
+    for i, ch in enumerate(ch[first_sample:last_sample] for ch in channels):
+        ax = subplots[i]
 
-    plot = True
-    for _ in range(((last_sample-first_sample)//samples_per_pic)):
+        ax.plot(t, ch, lw=4/call_number, label=file_name, alpha=0.3)
+        ax.set_xticks(np.arange(0, t[-1], 200))
+        ax.grid(which='minor', alpha=0.2)                                                
+        ax.grid(which='major', alpha=0.3)      
+        ax.set_title(next(channel_name))
+        ax.legend(loc=2, prop={'size': 10})
 
-        xlims = (start, end)
-        ylims = (-0.6, 0.6)
-
-        if not_plot_good_chunks:
-            curr_samples = np.arange(start, end)
-            intersect = np.intersect1d(curr_samples, fails, assume_unique=True)
-            plot = len(intersect) > 0
-
-        if plot:
-            if beats is not None:
-                idx = (beats >= start) & (beats <= end)
-                current_beats = beats[idx]
-
-            if predicted_beats is not None:
-                idx = (predicted_beats >= start) & (predicted_beats <= end)
-                current_predicted_beats = predicted_beats[idx]
-            
-            t = range(start, end)
-            fig = plt.figure(figsize=(25, len(channels)*5))
-
-            channel_name = iter(channel_names)
-            for i, ch in enumerate(ch[start:end] for ch in channels):
-                ax = fig.add_subplot(len(channels), 1, i+1)
-
-                ax.plot(t, ch, lw=1.0, c='b', label='original', alpha=0.3)
-                ax.axhline(y=0, ls='dashed', alpha=0.1, c='g')
-
-                if beats is not None:
-                    for b in current_beats:
-                        ax.axvline(b, lw=1, color='r', ls='dashdot', alpha=0.4)
-                        if events is not None:
-                            event = events[beats == b].ravel()
-                            if event.any():
-                                name = ('\n').join(np.array(diseases)[event != 0])
-                                ax.text(b, 0.9*ylims[1], name, fontsize=7, color='g',
-                                                     ha='center', rotation=60, alpha=0.7)       
-                        if predicted_events is not None:
-                            predicted_event = predicted_events[beats == b].ravel()
-                            if predicted_event.any():
-                                name = ('\n').join(np.array(predicted_diseases)[predicted_event != 0])
-                                ax.text(b, 0.2*ylims[1], name, fontsize=7, color='m',
-                                                     ha='center', rotation=60, alpha=1)     
-                        if probabilities is not None:   
-                            probability = probabilities[beats == b].ravel()
-                            text_probs = (' ').join('{:.2f}'.format(p) for p in probability)
-                            ax.text(b, -0.2*ylims[1], text_probs, fontsize=7, color='k',
-                                                 ha='center', rotation=60, alpha=0.7)                                      
-
-                if predicted_beats is not None:
-                    for b in current_predicted_beats:
-                        ax.axvline(b, lw=1, color='g', ls='dashed', alpha=0.4)
-
-                ax.set_xticks(np.arange(*xlims, step=20))                                                       
-                ax.set_yticks(np.arange(*ylims, step=0.1))                                                       
-                plt.setp(ax.get_xticklabels(), visible=False)
-                ax.grid(which='minor', alpha=0.2)                                                
-                ax.grid(which='major', alpha=0.3)      
-                ax.set_ylim(ylims)
-                ax.set_xlim(xlims)
-                ax.set_title(next(channel_name))
-                ax.legend(loc=2, prop={'size': 10})
-
-            fig.savefig(os.path.join(save_path, '{0}_{1}.png'.format(file_name, start)), bbox_inches='tight')
-            plt.close(fig)
-
-        start += samples_per_pic
-        end += samples_per_pic
-        if end > last_sample:
-            break
-
-    if verbose:
-        print('Finished plotting {}.'.format(file_name))
 
 
 def encode_dataset(path_to_cash, paths_to_Z_with_pattern, required_diseases):
@@ -260,7 +130,7 @@ def search_files_with_pattern(paths_to_dat, paths_to_Z):
 
 
 def plot_centroid(sample_index, beat_indexes, file_paths, save_path, path_to_data_files,
-    neighbor):
+    neighbor, fig_settings):
     beat_index = beat_indexes[sample_index]
 
     file_path = file_paths[sample_index]
@@ -271,19 +141,33 @@ def plot_centroid(sample_index, beat_indexes, file_paths, save_path, path_to_dat
 
     data = np.load(file_path).item()
     channels = tools.get_channels(data)
-    first_sample = data['beats'][beat_index-3 if beat_index-3>=0 else 0]
-    last_sample = data['beats'][beat_index+3 if beat_index+3<data['beats'].shape[0] else data['beats'].shape[0]-1]
-    plot_ecg(channels=channels, samples_per_pic=last_sample - first_sample,
-        save_path=save_path, file_name=file_name, first_sample=first_sample,
-        last_sample=last_sample, beats=data['beats'], events=data['events'],
-        diseases=all_holter_diseases)
+    first_sample = data['beats'][beat_index]-200
+    last_sample = data['beats'][beat_index]+200
+    if first_sample>=0 and last_sample<len(channels[0]):
+        plot_ecg(channels=channels, samples_per_pic=last_sample - first_sample,
+            save_path=save_path, file_name=file_name, fig_settings=fig_settings,
+            first_sample=first_sample,
+            last_sample=last_sample)
 
 def plot_centroids(n_neighbor, I, beat_indexes, file_paths, path_to_data_files):
     for i in range(len(I)):
         print('plot cluster', i)
+        fig = plt.figure(figsize=(20, 10))
+        subplots = [fig.add_subplot(3, 1 , i+1) for i in range(3)]
+        call_number = 1
+        fig_settings = [fig, subplots, call_number]
         for neighbor in range(n_neighbor):
+            fig_settings[2] = neighbor+1
+            save_path = 'Cluster_{}'.format(i)
             plot_centroid(I[i, neighbor], beat_indexes, file_paths,
-                'Cluster_{}'.format(i), path_to_data_files, neighbor)
+                save_path, path_to_data_files, neighbor, fig_settings)
+
+        file_path = file_paths[I[i, neighbor]]
+        file_path = file_path.split('_latent_state')[0]+'.npy'
+        file_name = tools.get_file_name(file_path)
+
+        fig.savefig('Cluster_{}.png'.format(i))
+        plt.close(fig)
 
 def get_paths_to_Z_with_pattern(path_to_cash, path_to_dat_files, path_to_Z):
     os.makedirs(os.path.dirname(path_to_cash), exist_ok=True)
@@ -305,6 +189,9 @@ def get_paths_to_Z_with_pattern(path_to_cash, path_to_dat_files, path_to_Z):
 
 if __name__ == '__main__':
 
+
+
+    
     ##############################
     # PARAMETERS
     path_to_Z = '/data/Work/Nazar/ECG_encoder/predictions/latent_states_PAC/'
@@ -313,10 +200,10 @@ if __name__ == '__main__':
     path_to_dat_files = '/data/Work/ZHR-Files/'
     required_diseases=['Atrial_PAC']
     ncentroids = 30
-    niter = 100
+    niter = 10
     verbose = True
     junctional_pattern = re.compile('АВ - блок')
-    n_neighbor = 5
+    n_neighbor = 10
     ##############################
 
     paths_to_Z_with_pattern = get_paths_to_Z_with_pattern(path_to_cash='cash/Z_paths.pickle',
